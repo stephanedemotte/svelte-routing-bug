667577582e81e985bc4c180eb31f7db6d73ea965
Stephane Demotte <stephanedemotte@Stephanes-MacBook-Pro.local>
2024-10-14 12:45:20 -0400

    big bang

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+const {children} = $props()
+import { onNavigate } from '$app/navigation'
+onNavigate(() => new Promise((r) => setTimeout(r, 5000)))
+</script>
+
+<nav><a href="/">home</a> <a href="/about">about</a></nav>
+
+{@render children()}
